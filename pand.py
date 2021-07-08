67071517d257d16b3f45081a9ee6dcc6a4790330
amy576 <amy.ho576@gmail.com>
2021-07-07 20:12:59 -0700

    add some data validation to creation of board

--- pand.py
+++ pand.py
@@ -2,13 +2,23 @@ class Board:
     def __init__(self, n, epicenter):
         ## size is the number of cities
         self.size = n
-        self.data = [0] * n
+        try:
+            self.data = [0] * n
+        except TypeError:
+            print("the first arugment must be an integer")
 
         ## epicenter is where the pandemic starts, with caseload 1
-        self.data[epicenter] = 1
+        try:
+            self.data[epicenter] = 1
+        except TypeError:
+            print("the second argument must be an integer")
         self.epicenter = epicenter
         print("Creating board: ", self.data)
 
+    ## this inspects a city
+    def move(self, city):
+        ## return the caseload discovered
+        print("Checking location ", city, ", Value = ", self.data[city])
 
 
 board = Board(10,2)
